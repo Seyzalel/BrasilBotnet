6987161535ab76d972600c2a4c83385ce45254cc
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-04-18 01:08:21 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import pyautogui, sys
 
 options = Options()
 options.headless = True
@@ -32,23 +31,19 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 })
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36', "acceptLanguage": "pt-BR,pt"})
 
+driver.get("https://www.example.com/")
+username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@name="username"]')))
+username_field.send_keys("seyzalel")
+
+password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@name="password"]')))
+password_field.send_keys("Sey17zalel17@$")
+
+entrar_button_xpath = '//div[contains(@class, "x9f619") and contains(text(), "Entrar")]'
+entrar_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, entrar_button_xpath)))
+entrar_button.click()
+
 try:
-    driver.get("https://www.instagram.com/accounts/login/")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@name="username"]')))
-    username_field = driver.find_element(By.XPATH, '//input[@name="username"]')
-    ActionChains(driver).move_to_element(username_field).click().perform()
-    pyautogui.typewrite("seyzalel", interval=0.2)
-
-    password_field = driver.find_element(By.XPATH, '//input[@name="password"]')
-    ActionChains(driver).move_to_element(password_field).click().perform()
-    pyautogui.typewrite("Sey17zalel17@$", interval=0.2)
-
-    entrar_button_xpath = '//div[contains(@class, "x9f619") and contains(text(), "Entrar")]'
-    entrar_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, entrar_button_xpath)))
-    ActionChains(driver).move_to_element(entrar_button).click().perform()
-
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//img[contains(@alt, "Foto do perfil de seyzalel")]')))
     print("Login bem sucedido.")
-except Exception as e:
-    print("Login mal sucedido.", e)
-    sys.exit(1)
+except:
+    print("Login mal sucedido.")
